7f5a5014f5041e559f45ca36e9dfcce909522621
gourab <gourab.saheb100@gmail.com>
2022-06-23 14:58:10 +0530

    Contacts

---
+++ AddressBook/Contacts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Contacts
+    {
+        public String firstname { get; set; }
+        public String lastname { get; set; }
+        public String Address { get; set; }
+        public double Zip { get; set; }
+        public double phoneno { get; set; }
+        public string email { get; set; }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,6 +1,17 @@
 ﻿
+using AddressBook;
+
+
+    Console.WriteLine("Welcome to Address Book Program in AddressBookMain class");
+
+Contacts persons=new Contacts();
+
+Console.WriteLine( persons.firstname = "Gourab");
+Console.WriteLine( persons.lastname = "Sarkar");
+Console.WriteLine( persons.Address = "Berhampore");
+Console.WriteLine( persons.Zip = 742103);
+Console.WriteLine( persons.phoneno = 6294212574);
+Console.WriteLine( persons.email = "gourab.saheb100@gmail.com");
 
 
 
-
-Console.WriteLine("Welcome to Address Book Program in AddressBookMain class");
